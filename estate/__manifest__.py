d659f8c711786f73d072169b56b6c75baeb004a0
Maris Sprancis <msprancis@gmail.com>
2024-01-27 21:36:17 +0200

    Working Real Estate screen, up to chapter 7

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,14 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0.0",  # Version
-    "application": True,  # This line says the module is an App, and not a module
+    "version": "17.0.0.1",  # Version
     "depends": ["base"],  # dependencies
-    "data": [
-
-    ],
     "installable": True,
-    'license': 'LGPL-3',
+    "application": True, # This line says the module is an App, and not a module
+    "license": "LGPL-3",
+    'category': 'Sales',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ]
 }
