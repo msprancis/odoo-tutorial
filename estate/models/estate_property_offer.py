f9add570aa5602e5a2dbc03a5d902aba3c9e35e7
Maris Sprancis <msprancis@gmail.com>
2024-01-28 15:38:37 +0200

    Fix offer tree view

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -4,7 +4,7 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Property Offer"
 
-    price = fields.Float(required=True, string="Price")
+    price = fields.Float(required=True)
     status = fields.Selection(selection=[('Accepted', 'Accepted'), ('Refused', 'Refused')], copy=False)
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate_property", required=True)
